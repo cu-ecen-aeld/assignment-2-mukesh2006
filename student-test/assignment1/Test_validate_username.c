904bbf918468d9761c76667bc3d9452236e1073c
mukesh2006 <mukesh2006@gmail.com>
2024-11-11 23:08:06 +0400

    adding first commit

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,7 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+    const char *uname_test=my_username();
+    char *uname_conf=malloc_username_from_conf_file();
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(uname_test,uname_conf, "Testing Unite Test 1");
 }
